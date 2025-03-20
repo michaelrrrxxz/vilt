f522907a7d24a76557e093da1b18c4907cad57a4
michaelrrrxxz <mangaoang.michaelangelo23@gmail.com>
2025-03-20 10:52:51 +0800

    add components

---
+++ resources/js/Layouts/FullLayout.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="h-screen w-full bg-blue-100">
+        <AppHeader />
+        <AppSidebar />
+        <AppContent>
+            <slot />
+        </AppContent>
+        <AppFooter />
+    </div>
+</template>
+<script setup>
+import AppHeader from '@/Layouts/core/Header.vue'
+import AppSidebar from '@/Layouts/core/Sidebar.vue'
+import AppContent from '@/Layouts/core/Content.vue'
+import AppFooter from '@/Layouts/core/Footer.vue'
+
+</script>
---
+++ resources/js/Layouts/core/Content.vue
@@ -0,0 +1,37 @@
+<template>
+    <main class="sm:ml-64 pt-[60px] px-[15px]">
+        <slot />
+    </main>
+</template>
+<script setup>
+import { onMounted } from 'vue';
+
+import { 
+    initAccordions, 
+    initCarousels, 
+    initCollapses, 
+    initDials, 
+    initDismisses, 
+    initDrawers, 
+    initDropdowns, 
+    initModals, 
+    initPopovers, 
+    initTabs, 
+    initTooltips
+} from 'flowbite';
+
+// initialize components based on data attribute selectors
+onMounted(() => {
+    initAccordions();
+    initCarousels();
+    initCollapses();
+    initDials();
+    initDismisses();
+    initDrawers();
+    initDropdowns();
+    initModals();
+    initPopovers();
+    initTabs();
+    initTooltips();
+});
+</script>
