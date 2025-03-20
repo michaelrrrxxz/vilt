41b86e3bd1018c82b16326e22cc60889ccab0548
michaelrrrxxz <mangaoang.michaelangelo23@gmail.com>
2025-03-20 08:51:05 +0800

    add ziggy

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,29 +1,28 @@
 import './bootstrap';
-import { createApp, h } from 'vue'
-import { createInertiaApp } from '@inertiajs/vue3'
+import '../css/app.css';
+
+import { createApp, h as renderComponent } from 'vue';
+import { createInertiaApp } from '@inertiajs/vue3';
+import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+import { ZiggyVue } from '../../vendor/tightenco/ziggy';
+
+const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
 createInertiaApp({
+    title: (title) => `${title} - ${appName}`,
+    resolve: (path) => resolvePageComponent(
+        `./Pages/${path}.vue`,
+        import.meta.glob('./Pages/**/*.vue')
+    ),
+    setup({ el, App, props, plugin }) {
+
+        return createApp({ render: () => renderComponent(App, props) })
+            .use(plugin)
+            .use(ZiggyVue)
+            .mount(el);
+    },
     progress: {
-        // The delay after which the progress bar will appear, in milliseconds...
-        delay: 250,
-
-        // The color of the progress bar...
-        color: '#29d',
-
-        // Whether to include the default NProgress styles...
-        includeCSS: true,
-
-        // Whether the NProgress spinner will be shown...
-        showSpinner: false,
-      },
-      // ...
-  resolve: name => {
-    const pages = import.meta.glob('./Pages/**/*.vue', { eager: true })
-    return pages[`./Pages/${name}.vue`]
-  },
-  setup({ el, App, props, plugin }) {
-    createApp({ render: () => h(App, props) })
-      .use(plugin)
-      .mount(el)
-  },
+        color: '#4B5563',
+        showSpinner: true
+    },
 })
